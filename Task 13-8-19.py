112bf10708ae2cce2ef09ce7dedcacbe94e4620a
Julia Scherbakova <110126906+JuliaScherbakova@users.noreply.github.com>
2022-09-16 19:05:29 +0300

    Update Task 13-8-19.py

--- Task 13-8-19.py
+++ Task 13-8-19.py
@@ -1,12 +1,12 @@
 tickets = int(input('Введите количество билетов: '))
-for i in range(0, tickets):
-    age = input('Введите возраст: ')
 # Создание списка для заполнения стоимости билетов в зависимости от возраста
 price = []
-for i in age:
-    if i in range(0, 18):
+for i in range(0, tickets):
+    age = int(input('Введите возраст: '))
+
+    if age < 18:
         price.append(0)
-    elif i in range(18, 25):
+    elif age >= 18 and age <= 25:
         price.append(990)
     else:
         price.append(1390)
@@ -15,4 +15,4 @@ for i in age:
 if tickets > 3:
     print("Сумма к оплате с учетом скидки: ", sum(price) - ((sum(price) / 100) * 10), "рублей")
 else:
-print("Сумма к оплате: ", sum(price), "рублей")
+    print("Сумма к оплате: ", sum(price), "рублей")"рублей")
