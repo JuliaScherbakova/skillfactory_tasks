d31c85b2b70cb0f6c669338e99598c63708d4773
Julia Scherbakova <110126906+JuliaScherbakova@users.noreply.github.com>
2022-11-22 21:23:22 +0300

    Update 17-9.py

--- 17-9.py
+++ 17-9.py
@@ -1,36 +1,39 @@
 array = list(map(int, input('введите числа через пробел: ').split()))
-
 print(array)
 
-for i in range(len(array)):  # проходим по всему массиву
-    idx_min = i  # сохраняем индекс предположительно минимального элемента
+
+for i in range(len(array)):
+    idx_min = i
     for j in range(i, len(array)):
         if array[j] < array[idx_min]:
             idx_min = j
-    if i != idx_min:  # если индекс не совпадает с минимальным, меняем
+    if i != idx_min:
         array[i], array[idx_min] = array[idx_min], array[i]
 print(array)
 
+
 element = int(input('Введите число в рамках последовательности: '))
 while element:
-    if element not in range(min(array) + 1,max(array)):
+    if element not in range(min(array) + 1, max(array)):
         print('Некорректное число')
         element = int(input('введите число в рамках последовательности: '))
     else:
         break
+array.append(element)
 
-        
+
 def binary_search(array, element, left, right):
     if left > right:
-        return False
+        return left, right
     middle = (right + left) // 2
     if array[middle] == element:
-        return middle
+        return middle - 1, middle
     elif element < array[middle]:
         return binary_search(array, element, left, middle - 1)
     else:
         return binary_search(array, element, middle + 1, right)
 
+
 a = (binary_search(array, element, 0, len(array)-1))
-# print(a-1, a) #работает, если число в списке
-# print(a) #работает, если числа нет в списке
+
+print(a)
